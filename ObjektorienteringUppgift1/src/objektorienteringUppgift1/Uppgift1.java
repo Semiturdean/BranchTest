ec664d59f140762d1238f8209bdfdeae52e67d95
Semi Turdean <Semiturdean@hotmail.com>
2017-09-27 15:23:06 +0200

    Update 27/09

--- ObjektorienteringUppgift1/src/objektorienteringUppgift1/Uppgift1.java
+++ ObjektorienteringUppgift1/src/objektorienteringUppgift1/Uppgift1.java
@@ -1,5 +1,19 @@
 package objektorienteringUppgift1;
 
-public class Uppgift1 {
-
+abstract class Uppgift1 {
+	Uppgift1(){}
+	private int vikt;
+	private String namn;
+	private String namnInput;
+	
+	public void Uppgift1(int vikt, String namn) {
+		this.vikt = vikt;
+		this.namn = namn;
+	}
+	public String getNamn() {
+		return namn;
+	}
+	public int getVikt() {
+		return vikt;
+	}
 }
