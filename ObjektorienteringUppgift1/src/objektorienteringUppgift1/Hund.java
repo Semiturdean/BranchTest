bcf718d29f52ea009960f740a2cd3e9603898971
Semi Turdean <Semiturdean@hotmail.com>
2017-09-27 15:33:02 +0200

    update

---
+++ ObjektorienteringUppgift1/src/objektorienteringUppgift1/Hund.java
@@ -0,0 +1,5 @@
+package objektorienteringUppgift1;
+
+public class Hund extends Uppgift1 {
+	
+}
--- ObjektorienteringUppgift1/src/objektorienteringUppgift1/Uppgift1.java
+++ ObjektorienteringUppgift1/src/objektorienteringUppgift1/Uppgift1.java
@@ -1,14 +1,17 @@
 package objektorienteringUppgift1;
 
+import javax.swing.JOptionPane;
+
 abstract class Uppgift1 {
 	Uppgift1(){}
 	private int vikt;
 	private String namn;
-	private String namnInput;
 	
-	public void Uppgift1(int vikt, String namn) {
+	public void setVikt(int vikt) {
 		this.vikt = vikt;
-		this.namn = namn;
+	}
+	public void setNamn() {
+		namn = JOptionPane.showInputDialog("Vad heter djuret som skall matas?");
 	}
 	public String getNamn() {
 		return namn;
