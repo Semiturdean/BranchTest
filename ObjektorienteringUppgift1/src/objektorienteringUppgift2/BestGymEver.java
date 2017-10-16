230da1cf7aa944620986207e253a728033b5a912
Semi Turdean <Semiturdean@hotmail.com>
2017-10-16 12:18:37 +0200

    progress yo

--- ObjektorienteringUppgift1/src/objektorienteringUppgift2/BestGymEver.java
+++ ObjektorienteringUppgift1/src/objektorienteringUppgift2/BestGymEver.java
@@ -1,9 +1,10 @@
 package objektorienteringUppgift2;
 
+import java.io.BufferedWriter;
 import java.io.FileNotFoundException;
+import java.io.FileWriter;
 import java.io.IOException;
 import java.io.PrintWriter;
-import java.nio.file.Files;
 import java.nio.file.Path;
 import java.nio.file.Paths;
 import java.time.LocalDate;
@@ -19,17 +20,15 @@ public class BestGymEver {
 		
 		
 		Path infilsPath = Paths.get("src\\objektorienteringUppgift2\\customers.txt");
-		Path utfilsPathToPayingCustomers = Paths.get("src\\objektorienteringUppgift2\\MembersWithActiveMembership.txt");
-		Path utfilsPathToActiveMembers = Paths.get("src\\objektorienteringUppgift2\\MembersWithRecentActivity.txt");
+		//Path utfilsPathToPayingCustomers = Paths.get("src\\objektorienteringUppgift2\\MembersWithActiveMembership.txt");
 		String firstLine;
 		String secondLine;
 		LocalDate date = LocalDate.now();
 		String aYearAgo = date.minusYears(1).toString();
 		String input = JOptionPane.showInputDialog("Ange namn eller personnummer på kunden!");
 		
-		
-		try (PrintWriter print = new PrintWriter(Files.newBufferedWriter(utfilsPathToPayingCustomers))){
-		Scanner readFile = new Scanner(infilsPath);
+		try (PrintWriter printRecentActivity = new PrintWriter(new BufferedWriter(new FileWriter("src\\objektorienteringUppgift2\\MembersWithRecentActivity.txt", true)))){
+			Scanner readFile = new Scanner(infilsPath);
 		
 		while(readFile.hasNext()) {
 			firstLine = readFile.nextLine();
@@ -40,7 +39,7 @@ public class BestGymEver {
 		if(Integer.parseInt(secondLine.replaceAll("-", "")) >= Integer.parseInt(aYearAgo.replaceAll("-", ""))) {
 			if(firstLine.contains(input)) {
 				System.out.println(firstLine + "\nMedlem med aktiv medlemskap\n");
-				print.format("%s" + "\nSenast aktiv: " + "%s" + "\n", firstLine, date);
+				printRecentActivity.printf("%s" + "\nSenast aktiv: " + "%s", firstLine, date);
 	
 		}
 		
