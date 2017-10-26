c20d0ce2eeff86698f407fa491f885f7eba4dc8d
Semi Turdean <Semiturdean@hotmail.com>
2017-10-26 09:30:52 +0200

    upd

--- ObjektorienteringUppgift1/src/objektorienteringUppgift3/BackendGame.java
+++ ObjektorienteringUppgift1/src/objektorienteringUppgift3/BackendGame.java
@@ -4,12 +4,14 @@ import java.awt.BorderLayout;
 import java.awt.FlowLayout;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
+import java.awt.event.MouseEvent;
+import java.awt.event.MouseListener;
 
 import javax.swing.JButton;
 import javax.swing.JFrame;
 import javax.swing.JPanel;
 
-public class BackendGame extends JFrame implements ActionListener {
+public class BackendGame extends JFrame implements ActionListener, MouseListener {
 	
 	private JPanel gamePanel = new JPanel();
 	private JPanel knappPanel = new JPanel();
@@ -36,6 +38,31 @@ public class BackendGame extends JFrame implements ActionListener {
 	}
 	@Override
 	public void actionPerformed(ActionEvent arg0) {
+		// TODO Auto-generated method stub
+		
+	}
+	@Override
+	public void mouseClicked(MouseEvent arg0) {
+		// TODO Auto-generated method stub
+		
+	}
+	@Override
+	public void mouseEntered(MouseEvent arg0) {
+		// TODO Auto-generated method stub
+		
+	}
+	@Override
+	public void mouseExited(MouseEvent arg0) {
+		// TODO Auto-generated method stub
+		
+	}
+	@Override
+	public void mousePressed(MouseEvent arg0) {
+		// TODO Auto-generated method stub
+		
+	}
+	@Override
+	public void mouseReleased(MouseEvent arg0) {
 		// TODO Auto-generated method stub
 		
 	}
